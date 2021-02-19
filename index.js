386bb1d86f799560844492f37563655fe8ceaebd
Chris Adams <chrisa@MacBook-Air.localdomain>
2021-02-18 20:45:46 -0800

    only log send error if there is one

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ server.on('message',function(msg,info){
 		//console.log('Message with message id %s does not exist', payload.MessageId);
 	}
 	server.send(msg, 18543, "127.0.0.1", (err) => {
-		console.log(err);
+		if(err) console.log(err);
 	});
 });
 
