1c9695bdd65deabe6a131a6c94b06d12de8158f5
Chris Adams <chrisa@MacBook-Air.localdomain>
2021-04-27 08:20:17 -0700

    fixing missed merge

--- payload/Msg_4232_CellNodeFull.js
+++ payload/Msg_4232_CellNodeFull.js
@@ -1,61 +1,5 @@
 ﻿const Parser = require('binary-parser').Parser;
 
-<<<<<<< HEAD
-	// Category    = Telemetry
-	// Object      = CellNodeItem
-	// Description = Cell node - full details
-	// MsgLength   = 52
-	// Version     = 2
-	// Frequency   = 300 mS
-	// Support     = Current
-	// Valid to    = SW 1.0.29
-	this.parse_4232 = function(msg) 
-	{		
-		var status = new Parser()
-		.skip(8)
-		.uint8('Cell_ID')
-		.uint8('Cell_USN')
-		.int16le('Cell_MinCellVolt',			{ formatter: (x) => {return x/1000;}})
-		.int16le('Cell_MaxCellVolt',			{ formatter: (x) => {return x/1000;}})
-		.uint8('Cell_MinCellTemp',			{ formatter: (x) => {return x-40;}}) // temperature ºC
-		.uint8('Cell_BypassTemp',			{ formatter: (x) => {return x-40;}}) // temperature ºC
-		.int16le('Cell_BypassAmp', 			{ formatter: (x) => {return x/1000;}})
-		.uint8('Cell_DataErrorCounter')
-		.uint8('Cell_ResetCounter')
-		.uint8('Cell_Status') /* Choices NodeStatuses
-				None = 0,
-				HighVolt = 1,
-				HighTemp = 2,
-				Ok = 3,
-				Timeout = 4,
-				LowVolt = 5,
-				Disabled = 6,
-				InBypass = 7,
-				InitialBypass = 8,
-				FinalBypass = 9,
-				MissingSetup = 10,
-				NoConfig = 11,
-				CellOutLimits = 12, */	
-		.uint8('Cell_IsOverdue')				// boolean 0 = Off , 1 = On
-
-		.int16le('Cell_LoCellVoltAlert',		{ formatter: (x) => {return x/1000;}})
-		.int16le('Cell_HiCellVoltAlert',		{ formatter: (x) => {return x/1000;}})
-		.int16le('Cell_BypassVoltLevel',		{ formatter: (x) => {return x/1000;}})
-		.int16le('Cell_BypassAmpLimit',		{ formatter: (x) => {return x/1000;}})
-		.uint8('Cell_BypassTempLimit',		{ formatter: (x) => {return x-40;}}) // temperature ºC
-		.uint8('Cell_HiCellTempAlert',		{ formatter: (x) => {return x-40;}}) // temperature ºC
-		.uint8('Cell_RawVoltCalOffset')
-		.int16le('Cell_FwVers')
-		.int16le('Cell_HwVers')
-		.int16le('Cell_BootVers')
-		.uint32le('Cell_SerialNo')	
-		.uint32le('Cell_BypassInitialDate') 	// Epoch
-		.floatle('Cell_BypassSessionAh',		{ formatter: (x) => {return x/1000;}}) // Ah
-		.uint8('Cell_RepeatCellV')
-		
-		return status.parse(msg);
-	}
-=======
 // Category    = Telemetry
 // Object      = CellNodeItem
 // Description = Cell node - full details
@@ -109,5 +53,4 @@ const status = new Parser()
 module.exports = function(msg)
 {
 	return status.parse(msg);
->>>>>>> 77538f80035fd93c4d8a526c70e1e0491ebeb8b7
 }
